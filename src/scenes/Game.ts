af39bf25427e7970083d0517701ad4aefaae39fa
hktari <bkamnik1995@gmail.com>
2024-10-16 13:22:27 +0200

    load assets into the scene

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,35 +1,50 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class Game extends Scene
-{
-    camera: Phaser.Cameras.Scene2D.Camera;
-    background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+export class Game extends Scene {
+  camera: Phaser.Cameras.Scene2D.Camera;
+  slotMachineBackground: Phaser.GameObjects.Image;
+  slotMachineSymbols: Phaser.GameObjects.Image[];
+  slotMachineLeverUp: Phaser.GameObjects.Image;
+  slotMachineLeverDown: Phaser.GameObjects.Image;
 
-    constructor ()
-    {
-        super('Game');
-    }
+  constructor() {
+    super("Game");
+  }
 
-    create ()
-    {
-        this.camera = this.cameras.main;
-        this.camera.setBackgroundColor(0x00ff00);
+  preload() {
+    this.load.image("leverUp", "assets/Slot Machine/lever-up.png");
+    this.load.image("leverDown", "assets/Slot Machine/lever-down.png");
+    this.load.image("reelBg", "assets/Slot Machine/reel-bg.png");
+    this.load.image(
+      "slotMachineBackground",
+      "assets/Slot Machine/slot-machine.png"
+    );
+    this.load.image("slotMachine1", "assets/Slot Machine/slot-machine1.png");
+    this.load.image("slotSymbol1", "assets/Slot Machine/slot-symbol1.png");
+    this.load.image("slotSymbol2", "assets/Slot Machine/slot-symbol2.png");
+    this.load.image("slotSymbol3", "assets/Slot Machine/slot-symbol3.png");
+    this.load.image("slotSymbol4", "assets/Slot Machine/slot-symbol4.png");
+    this.load.image("slotSymbol5", "assets/Slot Machine/slot-symbol5.png");
+  }
 
-        this.background = this.add.image(512, 384, 'background');
-        this.background.setAlpha(0.5);
+  create() {
+    this.camera = this.cameras.main;
+    this.camera.setBackgroundColor(0x00ff00);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+    this.slotMachineBackground = this.add.image(0, 0, "slotMachineBackground");
+    this.slotMachineSymbols = [
+      this.add.image(0, 0, "slotSymbol1"),
+      this.add.image(0, 0, "slotSymbol2"),
+      this.add.image(0, 0, "slotSymbol3"),
+      this.add.image(0, 0, "slotSymbol4"),
+      this.add.image(0, 0, "slotSymbol5"),
+    ];
 
-        this.input.once('pointerdown', () => {
+    this.slotMachineLeverUp = this.add.image(0, 0, "leverUp");
+    this.slotMachineLeverDown = this.add.image(0, 0, "leverDown");
 
-            this.scene.start('GameOver');
-
-        });
-    }
+    this.input.once("pointerdown", () => {
+      this.scene.start("GameOver");
+    });
+  }
 }
