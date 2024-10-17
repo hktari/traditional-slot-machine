ebfa361b5253a1d222a92655404983d3774f1244
hktari <bkamnik1995@gmail.com>
2024-10-17 21:57:25 +0200

    display symbols instead of rectangles

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { GameOver } from "./scenes/GameOver";
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  width: 816,
+  width: 1920,
   height: 624,
   parent: "game-container",
   backgroundColor: "#028af8",
--- src/scenes/GameOver.ts
+++ src/scenes/GameOver.ts
@@ -12,12 +12,29 @@ export class GameOver extends Scene {
   startLine: Phaser.GameObjects.Rectangle;
   container2: Phaser.GameObjects.Container;
 
+  private readonly symbolWidth = 100;
+
   constructor() {
     super("GameOver");
   }
 
+  preload() {
+    this.load.image("slotSymbol1", "assets/Slot Machine/slot-symbol1.png");
+    this.load.image("slotSymbol2", "assets/Slot Machine/slot-symbol2.png");
+    this.load.image("slotSymbol3", "assets/Slot Machine/slot-symbol3.png");
+    this.load.image("slotSymbol4", "assets/Slot Machine/slot-symbol4.png");
+    this.load.image("slotSymbol5", "assets/Slot Machine/slot-symbol5.png");
+  }
+
+  private symbols = [
+    "slotSymbol1",
+    "slotSymbol2",
+    "slotSymbol3",
+    "slotSymbol4",
+    "slotSymbol5",
+  ];
+
   private spacing = 10;
-  private rectangleWidth = 50;
   create() {
     this.camera = this.cameras.main;
     this.camera.setBackgroundColor(0xaabb00);
@@ -38,7 +55,7 @@ export class GameOver extends Scene {
     this.startLine = this.add.rectangle(
       0,
       384,
-      this.camera.width *  1/ 3,
+      (this.camera.width * 1) / 3,
       50,
       0x000000,
       1
@@ -46,42 +63,20 @@ export class GameOver extends Scene {
 
     this.startLine.setOrigin(0, 0.5);
 
-    this.container1 = this.add.container(
-      this.startLine.getBounds().right + this.rectangleWidth / 2,
+    this.container1 = this.createContainerWithSymbols(
+      this.startLine.getBounds().right + this.symbolWidth / 2,
       this.startLine.y
     );
-    for (let i = 0; i < 5; i++) {
-      const rectangle = this.add.rectangle(
-        i * (this.rectangleWidth + 10),
-        0,
-        this.rectangleWidth,
-        this.rectangleWidth,
-        0xaa4400,
-        1
-      );
-      this.container1.add(rectangle);
-    }
 
-    this.container2 = this.add.container(
+    this.container2 = this.createContainerWithSymbols(
       this.startLine.getBounds().right,
       this.startLine.y
     );
 
-    for (let i = 0; i < 5; i++) {
-      const rectangle = this.add.rectangle(
-        i * (this.rectangleWidth + this.spacing),
-        0,
-        this.rectangleWidth,
-        this.rectangleWidth,
-        0xaa4400,
-        1
-      );
-      this.container2.add(rectangle);
-    }
     this.container2.setX(
       this.container1.getBounds().left -
         this.container2.getBounds().width +
-        this.rectangleWidth / 2 -
+        this.symbolWidth / 2 -
         this.spacing
     );
     // const graphics = this.add.graphics();
@@ -95,13 +90,27 @@ export class GameOver extends Scene {
 
     const speed = 0.1;
 
-    this.startLine.setToTop()
-    this.finishLine.setToTop()
+    this.startLine.setToTop();
+    this.finishLine.setToTop();
 
     this.input.once("pointerdown", () => {
       this.animateXToFinishLine(this.container1, speed);
       this.animateXToFinishLine(this.container2, speed);
     });
+  }
+
+  createContainerWithSymbols(x: number, y: number) {
+    const container = this.add.container(x, y);
+    for (let i = 0; i < this.symbols.length; i++) {
+      const symbolImage = this.add.image(
+        i * this.symbolWidth + this.spacing,
+        0,
+        this.symbols[i]
+      );
+      container.add(symbolImage);
+    }
+
+    return container;
   }
 
   getRightMostContainer() {
@@ -122,7 +131,7 @@ export class GameOver extends Scene {
 
     this.tweens.add({
       targets: container,
-      x: this.finishLine.getBounds().left + this.rectangleWidth / 2,
+      x: this.finishLine.getBounds().left + this.symbolWidth / 2,
       duration,
       ease: "linear",
       onComplete: () => {
